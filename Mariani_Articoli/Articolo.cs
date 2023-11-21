5a3e23b0adc56a85c5e850f611a440f302f83d86
ludo <mariani.ludovico.studente@itispaleocapa.it>
2023-11-21 12:25:58 +0100

    Classe ArticoloNonAlimentare

--- Mariani_Articoli/Articolo.cs
+++ Mariani_Articoli/Articolo.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Mariani_Articoli
 {
-    internal class Class1
+    internal class Articolo
     {
+        protected int _codice;
+        protected string _descrizione;
+        protected double _prezzo;
+        protected bool _tessera;
+
+        public int Codice { get; set; }
+        public string Descrizione { get; set; }
+        public double Prezzo { get; set; }
+        public bool Tessera { get; set;}
+
+        public Articolo(int codice, string descrizione, double prezzo, bool tessera)
+        {
+            Codice = codice;
+            Descrizione = descrizione;
+            Prezzo = prezzo;
+            Tessera = tessera;
+        }
+
+        virtual public void Sconta()
+        {
+            if (Tessera)
+            {
+                Prezzo = Prezzo - ((5 * Prezzo) / 100);
+            }
+        }
     }
 }
--- Mariani_Articoli/ArticoloNonAlimentare.cs
+++ Mariani_Articoli/ArticoloNonAlimentare.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,22 @@ namespace Mariani_Articoli
 {
     internal class ArticoloNonAlimentare : Articolo
     {
+        private string _materiale;
+        private bool _riciclabile;
+
+        public string Materiale { get; set; }
+
+        public bool Riciclabile { get; set; }
+
+        public ArticoloNonAlimentare(int codice, string descrizione, double prezzo, bool tessera, string matariale, bool riciclabile) : base (codice, descrizione, prezzo, tessera) { Materiale = matariale; Riciclabile = riciclabile; }
+
+        public override void Sconta()
+        {
+            if (Tessera && ) 
+            {
+
+            }
+        }
 
     }
 }
