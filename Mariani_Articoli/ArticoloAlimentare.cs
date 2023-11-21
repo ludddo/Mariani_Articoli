77e1194174a744a4b4825d1596bc41e292bdb7db
ludo <mariani.ludovico.studente@itispaleocapa.it>
2023-11-21 12:31:41 +0100

    Classe ArticoloAlimentare

--- Mariani_Articoli/ArticoloAlimentare.cs
+++ Mariani_Articoli/ArticoloAlimentare.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Mariani_Articoli
 {
-    internal class Class1
+    internal class ArticoloAlimentare: Articolo
     {
+        private int _anno;
+
+        public int Anno { get; set; }
+
+        public ArticoloAlimentare(int codice, string descrizione, double prezzo, bool tessera, int anno) : base(codice, descrizione, prezzo, tessera) { Anno = anno; }
+
+        public override void Sconta()
+        {
+            if (Anno == DateTime.Now.Year && Tessera) 
+            {
+                Prezzo = Prezzo - ((25 * Prezzo) / 100);
+            }
+            if (Anno != DateTime.Now.Year && Tessera) 
+            {
+                Prezzo = Prezzo - ((5 * Prezzo) / 100);
+            }
+            if (Anno == DateTime.Now.Year && !Tessera) 
+            {
+                Prezzo = Prezzo - ((20 * Prezzo) / 100);
+            }
+        }
     }
 }
